57f8bae812a01921477748b74f41e19d77aaf329
Joshua Suskin <jsuskin85@gmail.com>
2026-09-08 19:32:31 -0400

    feat(auth): get user in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientInit from "./components/ClientInit";
 import Header from "./components/Header";
+import { getUser } from "@/utils/supabase/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,19 +20,22 @@ export const metadata: Metadata = {
   description: "Animal Pharmacy",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user, error } = await getUser();
+
+  if (error) console.error(error);
+
   return (
     <html lang='en' className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
         <meta name='apple-mobile-web-app-title' content='AnimalPharm' />
       </head>
       <body className='min-h-full flex flex-col'>
-        <Header />
-        <div className="h-16" />
+        <Header user={user} />
         <ClientInit />
         {children}
       </body>
