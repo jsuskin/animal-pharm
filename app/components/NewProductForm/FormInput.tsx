36936332fb28eb636d9523a4272cc13a0d5992fc
Joshua Suskin <jsuskin85@gmail.com>
2026-09-08 19:19:11 -0400

    feat: add required and autoComplete properties to form input

--- app/components/NewProductForm/FormInput.tsx
+++ app/components/NewProductForm/FormInput.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function FormInput({
   label,
   value,
@@ -5,13 +7,19 @@ export default function FormInput({
   type = "text",
   customStyles = "",
   placeholder = "",
+  icon,
+  autoComplete = "off",
+  required = false
 }: {
   label: string;
   value: string;
   setValue: (value: string) => void;
-  type?: "text" | "date" | "month" | "number";
+  type?: "text" | "date" | "month" | "number" | "email" | "password";
   customStyles?: string;
   placeholder?: string;
+  icon?: React.ReactNode;
+  autoComplete?: "off" | "on" | "current-password";
+  required?: boolean
 }) {
   const isNumeric = () => {
     switch (label) {
@@ -36,22 +44,24 @@ export default function FormInput({
       .join("");
 
   return (
-    <label
-      htmlFor={toKebabCase(label)}
-      className='flex flex-col my-3 mx-5 gap-1'
-    >
+    <label htmlFor={toKebabCase(label)} className='flex flex-col my-3 mx-5 gap-1'>
       <span className='text-slate-400 w-full'>{label}</span>
-      <input
-        name={toCamelCase(label)}
-        id={toKebabCase(label)}
-        type={type}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className={`border border-gray-400 py-1 px-2 text-slate-400 bg-slate-700 rounded-sm ${customStyles}`}
-        inputMode={isNumeric() ? "numeric" : "text"}
-        pattern={isNumeric() ? "[0-9]*" : ".*"}
-        placeholder={placeholder}
-      />
+      <div className='relative w-full'>
+        <input
+          name={toCamelCase(label)}
+          id={toKebabCase(label)}
+          type={type}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className={`border border-gray-400 w-full py-1 px-2 text-slate-400 bg-slate-700 rounded-sm ${customStyles}`}
+          inputMode={isNumeric() ? "numeric" : "text"}
+          pattern={isNumeric() ? "[0-9]*" : ".*"}
+          placeholder={placeholder}
+          required={required}
+          autoComplete={autoComplete}
+        />
+        {icon && <div className='absolute flex items-center right-2 top-1/2 -translate-y-1/2 text-3xl'>{icon}</div>}
+      </div>
     </label>
   );
 }
