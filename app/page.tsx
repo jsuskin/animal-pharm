414b566c45b2b17e050596964928078c135d3f1f
Joshua Suskin <jsuskin85@gmail.com>
2026-09-08 19:27:35 -0400

    feat: select products with derived quantities via sql action

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
   const supabase = await createClient();
-  const { data: products } = await supabase.from("products").select("*");
+  // const { data: products } = await supabase.from("products").select("*");
+  const { data: products } = await supabase.from("products_with_quantity").select("*");
 
   return (
     <div className='relative'>
